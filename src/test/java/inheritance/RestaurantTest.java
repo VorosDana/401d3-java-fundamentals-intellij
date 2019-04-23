677018336be5021f371d457f5fc8181158b82fdd
Dana Voros <vorosdana@gmail.com>
2019-04-23 11:07:42 -0700

    Added test for review star changes

--- src/test/java/inheritance/RestaurantTest.java
+++ src/test/java/inheritance/RestaurantTest.java
@@ -15,4 +15,17 @@ public class RestaurantTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void testReviewStarsChange() {
+        Restaurant testRest = new Restaurant("Golgor's Meats", 4);
+
+        testRest.addReview("Jill", 4, "PROTEIN");
+
+        assertEquals(4, testRest.getStars());
+
+        testRest.addReview("Bolzer", 2, "NO VEG");
+
+        assertEquals(3, testRest.getStars());
+    }
 }
